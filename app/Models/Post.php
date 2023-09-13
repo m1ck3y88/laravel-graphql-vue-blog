f5ebc9341d24e0759da5ca6893f94ddb053cc22b
Michael Pollard <mickeymakesapps88@gmail.com>
2023-09-13 08:45:05 -0600

    database migrations, relations and models complete

---
+++ app/Models/Post.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'lead',
+        'author_id',
+        'topic_id'
+    ];
+
+    public function topic(): BelongsTo
+    {
+        return $this->belongsTo(Topic::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+
+fetch('http://127.0.0.1:8000/graphql')
+    .then(response => response.json())
+    .then(data => console.log(data));
